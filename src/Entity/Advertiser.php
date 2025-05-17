b9488f292fc4942d1bd0731eb879b82984878492
HelbrWork <helbr.work@gmail.com>
2025-05-17 11:28:05 +0300

    add advertiser entity

---
+++ src/Entity/Advertiser.php
@@ -0,0 +1,87 @@
+<?php declare(strict_types=1);
+
+namespace App\Entity;
+
+use App\Repository\AdvertiserRepository;
+use DateTimeImmutable;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+use Gedmo\Timestampable\Traits\TimestampableEntity;
+
+#[ORM\Entity(repositoryClass: AdvertiserRepository::class)]
+class Advertiser
+{
+    use TimestampableEntity;
+
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: Types::INTEGER)]
+    private int|null $id = null;
+
+    #[ORM\Column(type: Types::STRING, length: 100, unique: true)]
+    private string $affiseAdvertiserId;
+
+    #[ORM\Column(type: Types::STRING, length: 100, unique: true)]
+    private string $affiseManagerId;
+
+    #[ORM\Column(type: Types::STRING, length: 100)]
+    private string $affiseTitle;
+
+    #[ORM\Column(type: Types::DATETIME_IMMUTABLE)]
+    private DateTimeImmutable $affiseCreatedAt;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function setId(?int $id): Advertiser
+    {
+        $this->id = $id;
+        return $this;
+    }
+
+    public function getAffiseAdvertiserId(): string
+    {
+        return $this->affiseAdvertiserId;
+    }
+
+    public function setAffiseAdvertiserId(string $affiseAdvertiserId): Advertiser
+    {
+        $this->affiseAdvertiserId = $affiseAdvertiserId;
+        return $this;
+    }
+
+    public function getAffiseManagerId(): string
+    {
+        return $this->affiseManagerId;
+    }
+
+    public function setAffiseManagerId(string $affiseManagerId): Advertiser
+    {
+        $this->affiseManagerId = $affiseManagerId;
+        return $this;
+    }
+
+    public function getAffiseTitle(): string
+    {
+        return $this->affiseTitle;
+    }
+
+    public function setAffiseTitle(string $affiseTitle): Advertiser
+    {
+        $this->affiseTitle = $affiseTitle;
+        return $this;
+    }
+
+    public function getAffiseCreatedAt(): DateTimeImmutable
+    {
+        return $this->affiseCreatedAt;
+    }
+
+    public function setAffiseCreatedAt(DateTimeImmutable $affiseCreatedAt): Advertiser
+    {
+        $this->affiseCreatedAt = $affiseCreatedAt;
+        return $this;
+    }
+}
